aef569b3b5382d3e9f6f78cfff7d84b921c21652
0x10000 <mohamedaitsimhand@gmail.com>
2020-09-28 23:49:24 +0100

    First push

---
+++ Cube Slider/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Whene ever we need to change from deffirent scenes or reload the current scene
+
+public class GameManager : MonoBehaviour
+{
+	bool gameHasEnded = false;
+	public GameObject completeLevelUI;
+	public void EndGame()
+	{
+		if (!gameHasEnded)
+		{
+			gameHasEnded = true;
+			Invoke("Restart", 1f);
+		}
+	}
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+	public void CompleteLevel()
+	{
+		completeLevelUI.SetActive(true);
+	}
+}
---
+++ Cube Slider/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float forwardForce = 1000f;
+	public float sideForce = 600f;
+	private bool noCollision = true;
+
+	// Fixed Update is called once per frame (it's better for calculating phiysices)
+	void FixedUpdate()
+	{
+		if (noCollision)
+		{
+			// Add a forward force on the x-axis
+			rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+			if (Input.GetKey("d"))
+				rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			if (Input.GetKey("a"))
+				rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			if (rb.position.y < -1)
+				FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+
+	// It will runs whenever the object collids with something
+	void OnCollisionEnter(Collision Info)
+	{
+		if (Info.collider.tag == "Obstacle")
+		{
+			noCollision = false;
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
